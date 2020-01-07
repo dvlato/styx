404ffd6c09c0d9a5f2db5e71feff50ce50a3ff23
David Latorre <a-dlatorre@hotels.com>
2020-01-07 17:37:39 +0000

    Test GitHub Actions,

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,4 +2,12 @@
 set -x
 
 echo "Deploying snapshot"
-mvn deploy --settings travis/mvn-settings.xml -B -U -P sonatype-oss-release -DskipTests=true -Dmaven.test.skip=true -Dgpg.skip=true
+
+
+if [ ! -n "$TRAVIS_TAG" ]; then
+  echo "Deploying release"
+  mvn deploy --settings travis/mvn-settings.xml -B -U -P sonatype-oss-release -DskipTests=true -Dmaven.test.skip=true -Dgpg.skip=false
+else
+  echo "Deploying snapshot"
+  mvn deploy --settings travis/mvn-settings.xml -B -U -P sonatype-oss-release -DskipTests=true -Dmaven.test.skip=true -Dgpg.skip=true
+fi
