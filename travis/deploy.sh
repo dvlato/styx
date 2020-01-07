3d2e2e8cde250e2a9ec430bfcda272bb9bac3121
David Latorre <a-dlatorre@hotels.com>
2020-01-07 15:17:39 +0000

    Do not cleanup?

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,5 +2,4 @@
 set -x
 
 echo "Deploying snapshot"
-echo "Username=$SONATYPE_JIRA_USERNAME , password=$SONATYPE_JIRA_PASSWORD"
-mvn clean deploy --settings travis/mvn-settings.xml -B -U -P sonatype-oss-release -DskipTests=true -Dmaven.test.skip=true -Dgpg.skip=true
+mvn deploy --settings travis/mvn-settings.xml -B -U -P sonatype-oss-release -DskipTests=true -Dmaven.test.skip=true -Dgpg.skip=true
