3c6d37d70c996dc9fcdbd9c5f504527912cbe128
David Latorre <a-dlatorre@hotels.com>
2020-01-07 19:23:59 +0000

    Build docker image in travis - speed up testing.

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,6 +1,4 @@
-#!/bin/bash -x
-set -x
-
+#!/bin/bash
 echo "Deploying snapshot"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 mvn deploy --settings travis/mvn-settings.xml -B -U -P sonatype-oss-release,docker -DskipTests=true -Dmaven.test.skip=true -Dgpg.skip=false
