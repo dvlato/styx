b3de81f650e4d86def08dc7e07c104f542f42f14
David Latorre <a-dlatorre@hotels.com>
2020-01-07 19:20:24 +0000

    Build docker image in travis.

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,12 +2,6 @@
 set -x
 
 echo "Deploying snapshot"
-
-
-if [ ! -n "$TRAVIS_TAG" ]; then
-  echo "Deploying release"
-  mvn deploy --settings travis/mvn-settings.xml -B -U -P sonatype-oss-release -DskipTests=true -Dmaven.test.skip=true -Dgpg.skip=false
-else
-  echo "Deploying snapshot"
-  mvn deploy --settings travis/mvn-settings.xml -B -U -P sonatype-oss-release -DskipTests=true -Dmaven.test.skip=true -Dgpg.skip=true
-fi
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+mvn deploy --settings travis/mvn-settings.xml -B -U -P sonatype-oss-release,docker -DskipTests=true -Dmaven.test.skip=true -Dgpg.skip=false
+docker push dlatorre/styx
